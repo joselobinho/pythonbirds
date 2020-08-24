9f92c210b3f02c66f730a469a03af337282b38e9
joselobinho <joselobinho@gmail.com>
2020-08-24 19:45:15 -0300

    Criando metodos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def comprimentar(self):
+        return(f'Ola Mundo {id(self)}')
+
+if __name__ == '__main__':
+    p=Pessoa()
+    print(p.comprimentar())
+    print(id(p))
